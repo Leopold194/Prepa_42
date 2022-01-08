adbfbd47b5869070e8faa88b5be621ab329ae4cc
Leopold <leopold.goudier@gmail.com>
2022-01-08 18:09:20 +0100

    Second try

--- Serie_1/ex02/ft_print_reverse_alphabet.c
+++ Serie_1/ex02/ft_print_reverse_alphabet.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
+#include <stdlib.h>
 
 void ft_print_reverse_alphabet(void)
 {
-  write(1, "zyxwvutsrqponmlkjihgfedcba\n", 28);
+  char alphabet[27];
+  alphabet[26] = '\n';
+  for (int i = 90; i >= 65; i--)
+    {
+      alphabet[abs(i-90)] = (char)i;
+    }
+  write(1, alphabet, 29);
 }
