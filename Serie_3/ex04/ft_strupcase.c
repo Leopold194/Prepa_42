2c43d93d7143f5986f56f042173a1883fb325a82
Leopold <leopold.goudier@gmail.com>
2022-03-09 15:34:06 +0100

    ex04 #1

---
+++ Serie_3/ex04/ft_strupcase.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+char *ft_strupcase(char *str)
+{
+  char littleAlphas[] = "abcdefghijklmnopqrstuvwxyz";
+  char bigAlphas[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  int i;
+  int j;
+  i = 0;
+  j = 0;
+
+  if (str != NULL)
+  {
+    while (str[i] != '\0')
+    {
+      if (str[i] == littleAlphas[j])
+      {
+	str[i] = bigAlphas[j];
+	j = 0;
+	i++;
+      }
+      else if (j > 26)
+      {
+	j = 0;
+	i++;
+      }
+      else
+      {
+	j++;
+      }
+    }
+  }
+  return str;
+}
---
+++ Serie_3/ex04/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+char *ft_strupcase(char *str);
+
+int main()
+{
+  char str[] = "Bonjour je suis Xx_ autruche14_xX !";
+  printf("%s",  ft_strupcase(str));
+  return 0;
+}
